a776d1bf82ffc6abddb76165d57184d6ebfbb3c7
Hepheir <hepheir@gmail.com>
2023-07-25 07:34:27 +0900

    Implement Base64 Encoded 이미지를 받아오는 기능

--- src/app.py
+++ src/app.py
@@ -1,14 +1,28 @@
+import base64
 import http
 
+import cv2
 import flask
+import numpy
 
 
 app = flask.Flask(__name__)
 
 
-@app.get('/')
+@app.post('/')
 def index():
-    return flask.Response(response='Hello, world!', status=http.HTTPStatus.OK)
+    app.logger.info(f'data received (length: {len(flask.request.data)})')
+
+    # Base 64 문자열로 들어온 이미지를 복호화
+    bytes_img = base64.b64decode(flask.request.data)
+    numpy_img = numpy.fromstring(bytes_img, dtype=numpy.uint8)
+    bgr_image = cv2.imdecode(numpy_img, cv2.IMREAD_COLOR)
+
+    # 이미지를 제대로 전달 받았는지 확인
+    app.logger.debug('요청으로 받은 이미지를 input.png 로 저장하고 있습니다.')
+    cv2.imwrite('input.png', bgr_image)
+
+    return flask.Response(status=http.HTTPStatus.OK)
 
 
 if __name__ == '__main__':
